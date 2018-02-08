1265f5074c4ec2f5b85daeed2b78da47b083d770
thirubala3171@ssh.imad.hasura-app.io <thirubala31@gmail.com>
2018-02-09 00:27:53 +0530

    Adding the query part

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,6 +1,6 @@
 from src import app
 from flask import jsonify, json
-from flask import Flask,request,render_template,make_response
+from flask import Flask,requests,render_template,make_response
 
 @app.route("/")
 def home():
@@ -16,3 +16,38 @@ def json_message():
     return jsonify(message="Hello World")
 	
 @app.route("/getmetric")
+def get_metric():
+	#query to select the data from the table
+	
+# This is the url to which the query is made
+	url = "https://data.declassification29.hasura-app.io/v1/query"
+
+	# This is the json payload for the query
+	requestPayload = {
+		"type": "select",
+		"args": {
+			"table": "world_happiness_report",
+			"columns": [
+				"*"
+			],
+			"order_by": [
+				{
+					"column": "ID"
+				}
+			]
+		}
+	}
+
+	# Setting headers
+	headers = {
+		"Content-Type": "application/json"
+	}
+
+	# Make the query and store response in resp
+	resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
+	# resp.content contains the json response.
+	print(resp.content)
+	
+
+
